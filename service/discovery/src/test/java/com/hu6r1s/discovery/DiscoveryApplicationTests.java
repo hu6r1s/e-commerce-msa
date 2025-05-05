1a279247121003ecc1104b07cac214dac645263d
Hu6r1s <darmaa333@gmail.com>
2025-05-05 23:23:47 +0900

    add(discovery): init

---
+++ service/discovery/src/test/java/com/hu6r1s/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hu6r1s.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
