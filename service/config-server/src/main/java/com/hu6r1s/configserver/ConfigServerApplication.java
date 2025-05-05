1f6bd352ea6af2729789c8e3b91579a8bd0fb9a7
Hu6r1s <darmaa333@gmail.com>
2025-05-05 23:13:21 +0900

    add(config-server): set

--- service/config-server/src/main/java/com/hu6r1s/configserver/ConfigServerApplication.java
+++ service/config-server/src/main/java/com/hu6r1s/configserver/ConfigServerApplication.java
@@ -2,6 +2,7 @@ package com.hu6r1s.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
