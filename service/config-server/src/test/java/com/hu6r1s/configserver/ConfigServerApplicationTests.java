0bacb73aa7c04b13881854ff0d9b83ffd57e5276
Hu6r1s <darmaa333@gmail.com>
2025-05-05 18:04:55 +0900

    add(config-server): set

---
+++ service/config-server/src/test/java/com/hu6r1s/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hu6r1s.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
