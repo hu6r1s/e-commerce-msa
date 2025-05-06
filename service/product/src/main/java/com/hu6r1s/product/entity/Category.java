6e026e05aaa1b83f9b391ea4c60bb9e9ccfaeac3
Hu6r1s <darmaa333@gmail.com>
2025-05-06 22:37:34 +0900

    add(product): product and category create database

---
+++ service/product/src/main/java/com/hu6r1s/product/entity/Category.java
@@ -0,0 +1,28 @@
+package com.hu6r1s.product.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+  @Id
+  @GeneratedValue
+  private Integer id;
+  private String name;
+  private String description;
+  @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<Product> products;
+}
