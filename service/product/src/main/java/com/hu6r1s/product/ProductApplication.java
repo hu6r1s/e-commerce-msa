1072b12a2e1c695365404eae1fd48a9d17a3e286
Hu6r1s <darmaa333@gmail.com>
2025-05-06 21:36:44 +0900

    add(product): product service set

---
+++ service/product/src/main/java/com/hu6r1s/product/ProductApplication.java
@@ -0,0 +1,13 @@
+package com.hu6r1s.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApplication.class, args);
+	}
+
+}
---
+++ service/product/src/test/java/com/hu6r1s/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hu6r1s.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
