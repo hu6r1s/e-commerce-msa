5a6597cbfe469ba3ff941cfe88473822ec349b31
Hu6r1s <darmaa333@gmail.com>
2025-05-06 11:51:45 +0900

    add(customer): customer service set

---
+++ service/customer/src/test/java/com/hu6r1s/customer/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hu6r1s.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
