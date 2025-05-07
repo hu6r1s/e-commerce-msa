cc205e6294042ead2c3255ad1fe7a2ab915aa887
Hu6r1s <darmaa333@gmail.com>
2025-05-07 20:51:31 +0900

    add(order): order set

---
+++ service/order/src/test/java/com/hu6r1s/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hu6r1s.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
