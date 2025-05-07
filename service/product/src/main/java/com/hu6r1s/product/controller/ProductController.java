43721021698aa49de1b4b48687b8843e4d137678
Hu6r1s <darmaa333@gmail.com>
2025-05-07 20:59:44 +0900

    refactor(customer, product): api prefix modified

--- service/product/src/main/java/com/hu6r1s/product/controller/ProductController.java
+++ service/product/src/main/java/com/hu6r1s/product/controller/ProductController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/product")
+@RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
 public class ProductController {
 
