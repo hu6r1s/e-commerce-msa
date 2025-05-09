e972260a2e6b6a2b210122b73d8d5f598fc7edd1
Hu6r1s <darmaa333@gmail.com>
2025-05-09 18:26:03 +0900

    add(payment): payment set

---
+++ service/payment/src/test/java/com/hu6r1s/payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hu6r1s.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
